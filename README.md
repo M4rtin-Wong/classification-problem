e98086057c02a05500d94df6b6d33db57b827741
Martin-wong-swivelsoftware <51832438+M4rtin-Wong@users.noreply.github.com>
2022-03-04 20:07:46 +0800

    classification using ANN

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # classification problem
  use ANN to classify the music data
+
+ Task1: load the data and label from data_task1.npy and label_task1.npy. X is the data, y is the target. Then create the class for ANN, use nn.Linear to apply a linear transformation to the data with 20 input and 16 output to the next layer and so on, the number of final output will be 4. Then define a function called forward to apply the element-wise function and store it in x for further usage. After that, use the loss function, CrossEntropyLoss for training the classification problem. 
+
+ Task2:load the data and label from data_task2.npy and label_task2.npy. X is the data, y is the target. Then, create the class for ANN, use nn.Linear to apply a linear transformation to the data with 100 input features and 16 output to the next layer and so on, the number of final output will be 16. Then define a function called forward to apply the element-wise function and store it in x for further usage. Also, in the “forward” function, flatten a contiguous range of dims in a tensor. After that, usee the loss function, CrossEntropyLoss for training the classification problem.
---
+++ test_task2.py
@@ -0,0 +1,78 @@
+import numpy as np
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from sklearn import datasets
+
+np.random.seed(0)
+torch.manual_seed(0)
+
+data_task2 = np.load('data_task2.npy')
+label_task2 = np.load('label_task2.npy')
+X = data_task2
+y = label_task2
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
+X_train = torch.FloatTensor(X_train)
+X_test = torch.FloatTensor(X_test)
+y_train = torch.LongTensor(y_train)
+y_test = torch.LongTensor(y_test)
+
+# use ANN to train the data
+class ANN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.fc1 = nn.Linear(in_features=100, out_features=64)
+        self.fc2 = nn.Linear(in_features=64, out_features=32)
+        self.output = nn.Linear(in_features=32, out_features=16)
+    def forward(self, x):
+        x = x.flatten(start_dim=1)
+        x = F.elu(self.fc1(x))
+        x = F.elu(self.fc2(x))
+        x = self.output(x)
+        return x
+   
+model = ANN()
+model
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+epochs = 100
+loss_arr = []
+train_loss = []
+train_accuracy = []
+
+for i in range(epochs): 
+    y_hat = model(X_train)
+    loss = criterion(y_hat, y_train)
+    
+    y_hat_class = np.argmax(y_hat.data.numpy(),axis=1)
+    accuracy = np.sum(y_train.data.numpy()==y_hat_class) / len(y_train) 
+    train_accuracy.append(accuracy)
+    train_loss.append(loss.item())
+    if i % 10 == 0:
+        print(f'Epoch: {i} Loss: {loss}')
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+    
+# plot the training loss and training accuracy
+fig, ax = plt.subplots(2, 1, figsize=(12,8))
+ax[0].plot(train_loss)
+ax[0].set_ylabel('Loss')
+ax[0].set_title('Training Loss')
+
+ax[1].plot(train_accuracy)
+ax[1].set_ylabel('Classification Accuracy')
+ax[1].set_title('Training Accuracy')
+
+plt.tight_layout()
+plt.show()
+
+y_hat_test = model(X_test)
+
+y_hat_class_test = np.argmax(y_hat_test.data.numpy(),axis=1)
+test_accuracy = np.sum(y_test.data.numpy()==y_hat_class_test) / len(y_test)
+
+print("Test Accuracy {:.2f}".format(test_accuracy))
